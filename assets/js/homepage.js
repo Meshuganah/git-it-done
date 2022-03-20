b95076f7c106612b0a8805f479ae3c7fb9f89c17
Josh Klein <kleinjjosh74@gmail.com>
2022-03-20 14:36:20 -0400

    displayed relevant api data to page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,71 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term"); 
+
+var getUserRepos = function(user) {
+    //Format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //Make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data) {
+            displayRepos(data, user);
+        });
+    });
 };
 
-getUserRepos();
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    //Get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+var displayRepos = function(repos, searchTerm) {
+    //Clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    //Loop over repos
+    for (i = 0; i < repos.length; i++) {
+        //Format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        //Create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        //Create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        //Create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        //Check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = 
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + "issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        //Append to container
+        repoEl.appendChild(titleEl);
+
+        repoEl.appendChild(statusEl);
+
+        //Append container to the DOM
+        repoContainerEl.appendChild(repoEl);
+    }
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
